e308d5085fabb8b97a142e95d936c075e22c0451
ShivamG <149230344+ShivamGMeta@users.noreply.github.com>
2024-06-08 21:21:19 +0530

    Add files via upload

---
+++ contracts/SmartConnection.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+//import "hardhat/console.sol";
+
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+    }
+
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
+
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
+
+        // make sure this is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // perform transaction
+        
+        balance += _amount;
+
+        // assert transaction completed successfully
+        assert(balance == _previousBalance + _amount);
+
+        // emit the event
+        emit Deposit(_amount);
+    }
+
+    // custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        // withdraw the given amount
+        balance -= _withdrawAmount;
+
+        // assert the balance is correct
+        assert(balance == (_previousBalance - _withdrawAmount));
+
+        // emit the event
+        emit Withdraw(_withdrawAmount);
+    }
+
+    function checkOwner() public pure returns (string memory){
+        string memory name = "Shivam Gupta";
+        return name;
+    }
+
+    function addition(uint var1, uint var2) public pure returns (uint) {
+        return var1 + var2;
+    
+    }
+
+    function subtraction(uint var1, uint var2) public pure returns (uint) {
+        require(var1>=var2, "Value of a must be grater than b");
+        return var1 - var2;
+    }
+
+    function multiplication(uint var1, uint var2) public pure returns (uint) {
+        return var1 * var2;
+    }
+
+}
+
