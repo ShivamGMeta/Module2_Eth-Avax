cc2d987f4a6e57cbf79599e137ebd446e637f3f6
ShivamG <149230344+ShivamGMeta@users.noreply.github.com>
2024-06-28 17:56:18 +0530

    Update SmartConnection.sol

--- contracts/SmartConnection.sol
+++ contracts/SmartConnection.sol
@@ -1,45 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public interestRate; // in basis points, e.g., 700 = 7%
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event OwnerChanged(address indexed oldOwner, address indexed newOwner);
+    event InterestRateChanged(uint256 oldRate, uint256 newRate);
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
+    constructor(uint initBalance, address payable initialOwner, uint256 initialInterestRate) payable {
+        owner = initialOwner;
         balance = initBalance;
+        interestRate = initialInterestRate;
     }
 
-    function getBalance() public view returns(uint256){
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
+
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
+    function deposit(uint256 _amount) public payable onlyOwner {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+    function withdraw(uint256 _withdrawAmount) public onlyOwner {
         uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
@@ -47,14 +43,30 @@ contract Assessment {
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function setOwner(address payable _newOwner) public onlyOwner {
+        require(_newOwner != address(0), "New owner cannot be the zero address");
+        address oldOwner = owner;
+        owner = _newOwner;
+        emit OwnerChanged(oldOwner, _newOwner);
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    function setInterestRate(uint256 _newRate) public onlyOwner {
+        require(_newRate >= 0, "Interest rate cannot be negative");
+        uint256 oldRate = interestRate;
+        interestRate = _newRate;
+        emit InterestRateChanged(oldRate, _newRate);
+    }
+
+    function getInterestRate() public view returns (uint256) {
+        return interestRate;
     }
 }
